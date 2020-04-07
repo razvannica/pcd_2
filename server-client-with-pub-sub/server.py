45b10319ecf9c4039a27a1904baf2514717033d3
razvan.nica.home <justme1SKYLINE>
2020-04-07 14:10:46 +0300

    feat(pcd_2): Adds integration solution for pub/sub
    PCD_Homework2

---
+++ server-client-with-pub-sub/server.py
@@ -0,0 +1,60 @@
+import asyncio
+import json
+import requests
+
+import websockets
+
+users = set()
+
+
+async def send_message(message):
+    r"""Posts message to Google Pub/Sub server """
+    res = requests.post('http://localhost:6666/pubsub/test', json={"data": message})
+    if res.ok:
+        return res.json()
+
+
+# notifica userii ca un user a intrat sau a iesit din aplicatie
+# trebuie sa mai lucrez la functia asta
+# async def notify_users():
+#     if users:
+#         message = "New user connected"
+#         await asyncio.wait([user.send(message) for user in users])
+
+
+# async def send_users_message(message):
+#     await asyncio.wait([user.send(message) for user in users])
+
+
+async def register(websocket):
+    users.add(websocket)
+    # await notify_users()
+
+
+async def unregister(websocket):
+    users.remove(websocket)
+    print("Client removed")
+    # await notify_users()
+
+
+async def handle(websocket, path):
+    await register(websocket)
+    async for message in websocket:
+        data = json.loads(message)
+        if data["user"] is not None and data["mess"] is None:
+            print("User connected :  ", data["user"])
+            await send_message("User " + data["user"] + " now connected.")
+            # await send_users_message("User " + data["user"] + " now connected.")
+
+        if data["mess"] is not None:
+            print("Message from " + data["user"] + ":" + data["mess"])
+            await send_message("Message from " + data["user"] + " : " + data["mess"])
+            # await send_users_message("Message from " + data["user"] + " : " + data["mess"])
+
+    await unregister(websocket)
+
+
+start_server = websockets.serve(handle, "localhost", 1234)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
---
+++ server-client-with-pub-sub/sub.py
@@ -0,0 +1,64 @@
+import asyncio
+import os
+
+import websockets
+from google.cloud import pubsub
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'key.json'
+proj_name = 'pcd2-272020'
+sub_name = 'test'
+
+
+def callback(message):
+    import time
+    ts = int(time.time())
+    f = open("temp/message-" + str(ts) + ".txt", "a")
+    f.write(message.data.decode("utf-8"))
+    f.close()
+    #
+    # if message.attributes:
+    #     print('Attributes:')
+    #     for key in message.attributes:
+    #         value = message.attributes.get(key)
+    #         print('{}: {}'.format(key, value))
+    message.ack()
+
+
+def get_messages():
+    import glob
+    files = glob.glob("temp/*")
+    messages = []
+    for file in files:
+        f = open(file, "r")
+        messages.append(f.read())
+        f.close()
+        import os
+        os.remove(file)
+    return messages
+
+
+async def send_messages_to_clients(websocket):
+    messages = get_messages()
+    for message in messages:
+        print("asfgsdag")
+        await websocket.send('Received message: {}'.format(message))
+
+
+async def sub_pull(websocket, path):
+    subscriber = pubsub.SubscriberClient()
+    subscription_path = subscriber.subscription_path(
+        proj_name, sub_name)
+
+    subscriber.subscribe(subscription_path, callback=callback)
+    print('Listening for messages on: {}'.format(subscription_path))
+    while True:
+        await send_messages_to_clients(websocket)
+
+
+"""
+Client awaiting for Google PUB/Sub messages
+"""
+start_server = websockets.serve(sub_pull, "localhost", 5678)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
